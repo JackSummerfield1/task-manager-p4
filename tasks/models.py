30f509b102492f81c9ec72663b5519eb93900c66
Jack Summerfield <summerzj10@gmail.com>
2024-11-28 21:02:06 +0000

    feat: create Task model and register it to admin page

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+PRIORITY = ((0, "Low"), (1, "Medium"), (3, "High"))
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    priority = models.IntegerField(choices=PRIORITY, default=0)
+    completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
