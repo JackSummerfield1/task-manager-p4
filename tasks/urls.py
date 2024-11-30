5daf6b5dd7a4fc591132f38da7c67e26355378ed
Jack Summerfield <summerzj10@gmail.com>
2024-11-30 12:33:49 +0000

    feat: construct url path for deleting a task

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.task_list, name='task_list'),
     path('task/create/', views.task_create, name='task_create'),
     path('task/edit/<int:pk>/', views.task_edit, name='task_edit'),
+    path('task/delete/<int:pk>/', views.task_delete, name='task_delete'),
 ]
