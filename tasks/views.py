6f7d7b185738d816d5f60f54ef0bad69ed2324b4
Jack Summerfield <summerzj10@gmail.com>
2024-11-30 12:06:32 +0000

    feat: create the edit_task view

--- tasks/views.py
+++ tasks/views.py
@@ -33,3 +33,16 @@ def task_create(request):
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form})
+
+
+@login_required
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm(instance=task)
+    return render(request, 'tasks/task_form.html', {'form': form})
