4f62824be1560f1af00a63faaa3ad04d0846a4a2
Jack Summerfield <summerzj10@gmail.com>
2024-11-30 13:00:15 +0000

    feat: updating the placeholder attr on the form page

--- tasks/forms.py
+++ tasks/forms.py
@@ -8,3 +8,13 @@ class TaskForm(forms.ModelForm):
         model = Task
         fields = ['user', 'title', 'description',
                   'deadline', 'priority', 'completed']
+
+    # Full credit to Pretty Printed on how to style Django Form Fields,
+    # specifically the placeholder attr
+    # https://www.youtube.com/watch?v=ynToND_xOAM
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['deadline'].widget.attrs.update(
+            {'placeholder': 'YYYY-MM-DD HH:MM:SS'})
+        self.fields['description'].widget.attrs.update(
+            {'placeholder': 'Please provide a detailed description. (Optional)'})
