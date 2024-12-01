8547880c634aff37744b9675d23a0739d025f248
Jack Summerfield <summerzj10@gmail.com>
2024-12-01 22:14:55 +0000

    feat: prepare test environ with a test user and a task

--- tasks/test_views.py
+++ tasks/test_views.py
@@ -2,3 +2,13 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from .models import Task
+
+
+class TestTaskViews(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_superuser(
+            username='testSuperuser', password='testPassword', email='test@email.com')
+        self.task = Task.objects.create(user=self.user, name='test task')
+
+    def login(self):
+        self.client.login(username='testSuperuser', password='testPassword')
