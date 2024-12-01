e3864c54fe7e6a0d68a2a37d7cf880855aa7b6a1
Jack Summerfield <summerzj10@gmail.com>
2024-12-01 10:14:54 +0000

    feat: load templates dir, allows editing of allauth temps

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'tasks/templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -83,7 +84,7 @@ ROOT_URLCONF = 'task_manager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
