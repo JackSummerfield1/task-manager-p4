51c2501db7db26c67ebf8de834cd0044a4a36ba2
Jack Summerfield <summerzj10@gmail.com>
2024-12-01 16:19:25 +0000

    feat: add test to ensure form is invalid with invalid deadline

--- tasks/test_forms.py
+++ tasks/test_forms.py
@@ -30,3 +30,18 @@ class TestTaskForm(TestCase):
         })
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors['title'], ['This field is required.'])
+
+    def test_form_invalid_with_invalid_deadline(self):
+        """
+        This tests that the form is invalid with an invalid deadline
+        """
+        form = TaskForm({
+            'title': 'Test Task',
+            'description': 'This is a test description.',
+            'deadline': 'dbajdba',
+            'priority': 1,
+            'completed': False
+        })
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['deadline'], [
+                         'Enter a valid date/time.'])
